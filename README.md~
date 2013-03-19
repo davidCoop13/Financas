087b8e4051d1ff1dd287d6dd20acd023d96fb8cc
Daniel Teixeira Silva <eugostodequeijadinhadequeijo+github@gmail.com>
2013-03-19 02:14:51 -0300

    Resetando projeto

--- README.md~
+++
@@ -1,3 +0,0 @@
-Laboratório de Engenharia de Software II
-
-Projeto de Finanças do ERP.
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Template::Application.config.secret_token = '6a865d89e47047859e2d053e3d65c7ce5a7e95857905b291c90bbf0394125dcae197b96e001e6c4a1f0535d59185a1014b54f91976f1270f75f5a3c3f6fd2142'
