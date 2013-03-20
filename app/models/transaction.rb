b53754c43c040b9b7206eee57e9378603c69fc4f
Camilo Rodegheri <camilo.rodegheri@gmail.com>
2013-03-19 22:20:23 -0300

    Ajustes

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,4 @@
 class Transaction < ActiveRecord::Base
   belongs_to :account
-  attr_accessible :amount, :credit, :date, :description, :number
+  attr_accessible :amount, :credit, :date, :description, :number, :account_id
 end
