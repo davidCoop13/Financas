7ff9da1f83f06cdda7340aa6e82993f21b6e57fa
Camilo Rodegheri <camilo.rodegheri@gmail.com>
2013-03-26 16:15:10 -0300

    Atualizações nas views

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'rails', '3.2.8'
 gem 'mysql2'
 gem 'yaml_db'
 
+gem 'breadcrumbs'
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :add_initial_breadcrumbs
+
+  private
+  def add_initial_breadcrumbs
+    #breadcrumbs.add 'Home', root_path
+  end
 end
