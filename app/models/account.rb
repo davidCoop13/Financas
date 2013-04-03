ee5cc3dcaa0c9b94de6924ab4db49fe3a5c4d6fd
Daniel Teixeira Silva <eugostodequeijadinhadequeijo+github@gmail.com>
2013-04-03 15:48:52 -0300

    Accounts e Categories pelo DAVID

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,23 @@
 class Account < ActiveRecord::Base
   attr_accessible :name, :number
 
-  has_many :transaction
+  has_many :transactions
   has_many :bill
-  has_many :invoice  
+  has_many :invoice 
+
+def SomaTotalTransacoes
+	@somaTransactions = 0
+	self.transactions.each do |arrayOfTransactions|
+		@somaTransactions+=arrayOfTransactions.amount
+	end
+	return @somaTransactions
+end
+
+
+#def Soma
+#	@somaTransactions= Transaction.all(:select => "transaction.account_id==Account_id, 
+#			SUM(transaction.amount) as transaction_total")
+#		@somaTransactions.each do |transaction|
+# 		transaction.transaction_total
+#		end
 end
